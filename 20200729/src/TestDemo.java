e9e4fe6f2e3090bb6f5d885446edbe2f6fb261de
Fighting261 <1227873949@qq.com>
2020-07-29 21:23:06 +0800

    找出数组中出现次数超过一半的数字

---
+++ 20200729/src/TestDemo.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/**
+ * @ program: 20200729
+ * @ Description：
+ * @ CreateTime：2020/7/29 20:31
+ * @ Author：Mr Zhang
+ */
+
+public class TestDemo {
+
+/*  数组中有一个数字出现的次数超过数组长度的一半,请找出这个数字。例如输入一个长度为9的数组 {1,2,3,2,2,2,5,4,2}。
+    由于数字2在数组中出现了5次,超过数组长度的一半,因此输出2。如果不存在则输 出0。
+    */
+    public int MoreThanHalfNum_Solution(int [] array) {
+        int index = array[0];
+        int count = 1;
+        for(int i = 1; i < array.length; i++) {
+            if(index == array.length) {
+                count++;
+            }else if(count == 0) {
+                index = array[i];
+                count = 1;
+            }else {
+                count--;
+            }
+        }
+        count = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(index == array[i]) {
+                count++;
+            }
+        }
+        return count*2 > array.length ? index:0;
+    }
+}
